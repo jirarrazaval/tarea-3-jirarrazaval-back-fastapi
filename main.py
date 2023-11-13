c65acea72ec11155ac96f138c8f07e64d85b8309
JAVIERA IRARRAZAVAL <jirarrzaval@uc.cl>
2023-11-12 22:53:48 -0300

    3 eps

--- main.py
+++ main.py
@@ -1,34 +1,127 @@
 from fastapi import FastAPI, HTTPException
 from google.cloud import storage
 from google.cloud.exceptions import NotFound
+import xml.etree.ElementTree as ET
+import json
 
 app = FastAPI()
+
+# Me conecto al bucket de Google Cloud Storage
+bucket_name = '2023-2-tarea3'
+client = storage.Client.from_service_account_json('tarea3-service-key.json')
+bucket = client.bucket(bucket_name)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/files/")
 async def list_files():
-    bucket_name = '2023-2-tarea3'
-
     try:
-        # Authenticate using the service account key
-        client = storage.Client.from_service_account_json('tarea3-service-key.json')
-
-        # Get a reference to the bucket
-        bucket = client.bucket(bucket_name)
-
-        # List all files in the bucket
         files = bucket.list_blobs()
-
-        # Extract file information
-        file_list = [{'name': file.name, 'url': f'https://storage.googleapis.com/{bucket_name}/{file.name}'} for file in files]
-
+        file_list = [{'name': file.name, 'url': f'/{bucket_name}/{file.name}'} for file in files]
         return {"files": file_list}
 
     except NotFound:
         raise HTTPException(status_code=404, detail="Bucket not found")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# 1. Aircrafts.xml
+@app.get("/files/aircrafts")
+async def get_aircrafts():
+    try:
+        blob = bucket.get_blob('aircrafts.xml')
+        aircrafts_data = blob.download_as_string().decode('utf-8')
+
+        # Parse XML
+        root = ET.fromstring(aircrafts_data)
+        aircraft_list = []
+        for aircraft in root:
+            aircraft_dict = {}
+            for attribute in aircraft:
+                aircraft_dict[attribute.tag] = attribute.text
+            aircraft_list.append(aircraft_dict)
+
+        return {"aircrafts": aircraft_list}
+
+    except NotFound:
+        raise HTTPException(status_code=404, detail="File not found")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    
+# 2. Airports.csv
+@app.get("/files/airports")
+async def get_airports():
+    try:
+        blob = bucket.get_blob('airports.csv')
+        airports_data = blob.download_as_string().decode('utf-8').split('\n')
+
+        # Extract headers and airport data
+        headers = airports_data[0].split(',')
+        airport_data = [line.split(',') for line in airports_data[1:] if line]
+
+        # Organize data by attributes
+        airport_list = []
+        for entry in airport_data:
+            airport_dict = {headers[i]: entry[i] for i in range(len(headers))}
+            airport_list.append(airport_dict)
+
+        return {"airports": airport_list}
+
+    except NotFound:
+        raise HTTPException(status_code=404, detail="File not found")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# 3. Flight Detail
+@app.get("/files/flights/{year}/{month}/")
+async def get_flight_data(year: int, month: int):
+    try:
+        # Generate the path to the flight data file
+        file_path = f'flights/{year:04d}/{month:02d}/flight_data.json'
+        blob = bucket.get_blob(file_path)
+
+        # Check if the file exists
+        if blob is None:
+            raise HTTPException(status_code=404, detail="Flight data not found")
+
+        # Download and parse JSON data
+        file_content = blob.download_as_text()
+
+        # Check if the file is empty
+        if not file_content:
+            raise HTTPException(status_code=404, detail="Flight data is empty")
+
+        flight_data = json.loads(file_content)
+
+        # Organize the flight data by attributes
+        organized_data = []
+        for flight in flight_data:
+            organized_flight = {
+                "flightNumber": flight["flightNumber"],
+                "originIATA": flight["originIATA"],
+                "destinationIATA": flight["destinationIATA"],
+                "airline": flight["airline"],
+                "aircraftID": flight["aircraftID"],
+            }
+            organized_data.append(organized_flight)
+
+        return {"flights_data": organized_data}
+
+    except NotFound:
+        raise HTTPException(status_code=404, detail="Flight data not found")
+
+    except HTTPException as e:
+        # Re-raise HTTPException to pass through custom exceptions
+        raise
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
